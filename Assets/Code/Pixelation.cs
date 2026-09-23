b8639a03946d65154612f02b839fed5cc901e910
Axolay <147523537+GreedyAllay@users.noreply.github.com>
2026-09-23 12:35:26 +0200

    some shit

--- Assets/Code/Pixelation.cs
+++ Assets/Code/Pixelation.cs
@@ -3,14 +3,15 @@ using UnityEngine;
 public class Pixelation : MonoBehaviour {
     public RenderTexture renderTexture;
 
-    public int width = 320;
     public int height = 180;
 
     void Start() {
-        renderTexture.width = width;
-        renderTexture.height = height;
+        float aspect = (float)Screen.width / Screen.height;
+        int width = Mathf.RoundToInt(height * aspect);
 
         renderTexture.Release();
+        renderTexture.width = width;
+        renderTexture.height = height;
         renderTexture.Create();
     }
 }
